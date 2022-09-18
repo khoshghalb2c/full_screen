a1e1a6ea6d47619e6bf6d850fd222ff0ba1225dc
SAZL.IR <khoshghalb2c@gmail.com>
2022-09-18 12:12:27 +0430

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+/*!
+  * sFULLSCREEN > owner is https://sazl.ir/;
+  * Copyright 2020-2021;
+  */
+  function openSfullscreen(x){
+    if(x.requestFullscreen){
+      x.requestFullscreen();
+    }else if(x.mozRequestFullScreen){
+      x.mozRequestFullScreen();
+    }else if(x.webkitRequestFullscreen){
+      x.webkitRequestFullscreen();
+    }
+  }
+  function closeSfullscreen(x){
+    if(x.exitFullscreen){
+      x.exitFullscreen();
+    }else if(x.webkitExitFullscreen){
+      x.webkitExitFullscreen();
+    }else if(x.msExitFullscreen){
+      x.msExitFullscreen();
+    }
+  }
+/*!* POWERED BY SAZL.IR *!*/
